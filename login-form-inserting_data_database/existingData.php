a093fc7dad878ad19bd22548f908b455aac93813
Mubasshir08 <77483719+Mubasshir08@users.noreply.github.com>
2022-09-20 23:59:51 -0700

    Existing  Data checking

---
+++ login-form-inserting_data_database/existingData.php
@@ -0,0 +1,16 @@
+<?php
+// including connection
+include ('dbcon.php');
+
+// checking the existing data
+if(isset($_POST['submit-btn'])){
+    $name = $_POST['username'];
+    $pass = $_POST['password']; 
+    $query = "SELECT * FROM users WHERE name = '$name' AND password = '$pass'";
+    $query_run = mysqli_query($conn, $query);
+    if($query_run){
+       // print_r($query_run);
+        echo 'successfully founded data';
+       // print_r (mysqli_num_rows($query_run)); it's filter the actual matching data row 
+    }
+}
